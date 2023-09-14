a9b53ac87b21abdc248bbc14304bf120a66c7ced
Ajiboye-01 <ariyoajiboye0@gmail.com>
2023-09-14 12:08:50 +0100

    Database conn

---
+++ src/main/java/com/fintech/globalBank/Entity/User.java
@@ -0,0 +1,47 @@
+package com.fintech.globalBank.Entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "bank_user")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String email;
+    private String phoneNumber;
+    private String altPhoneNumber;
+    private String Status;
+    private String gender;
+    private String address;
+    private String stateOfOrigin;
+    private String accountNumber;
+    private BigDecimal accountBalance;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+}
